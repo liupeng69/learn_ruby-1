37dac44d03805347abb05dfcc4b750db636c6b20
lponline21 <lponline21@gmail.com>
2017-09-28 01:23:16 +0000

    Finished 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,14 @@
 #write your code here
+
+def echo(message)
+  return message
+end
+
+def shout(message)
+  return message.upcase
+end
+
+def repeat(message)
+  return message + " " + message
+end
+
