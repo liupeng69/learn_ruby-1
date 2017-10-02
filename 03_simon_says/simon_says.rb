7ffb45185a3a9941d7e4f3fc437c90e116b4cd66
lponline21 <lponline21@gmail.com>
2017-10-02 17:27:21 +0000

    finished 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,14 +1,34 @@
 #write your code here
 
-def echo(message)
-  return message
+def echo(s)
+  return s
 end
 
-def shout(message)
-  return message.upcase
+def shout(s)
+  return s.upcase
 end
 
-def repeat(message)
-  return message + " " + message
+def repeat(s, times = 2)
+  return ([s] * times).join(" ")
+end
+
+def start_of_word(s, n)
+  s[0...n]
+end
+
+def first_word(s)
+  s.split(" ").first
+end
+
+def titleize(s)
+  words = s.split.map do |word|
+    if %w(the and over).include?(word)
+      word
+    else
+      word.capitalize
+    end
+  end
+  words.first.capitalize!
+  words.join(" ")
 end
 
